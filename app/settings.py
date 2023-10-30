b3048317e7401cd852eb2e862be019a25474c8fb
Jared Morse <jarcoal@gmail.com>
2023-10-29 22:24:07 -0700

    Fix static files command

--- app/settings.py
+++ app/settings.py
@@ -195,6 +195,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
